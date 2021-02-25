8ceab7abd0b1dae95d2dc03922d92ff9ba586f2b
0xVoltron <0xvoltron@protonmail.com>
2021-02-25 22:13:26 +0200

    Set dark mode by default if darkMode is not defined

--- src/core/apollo/variables.js
+++ src/core/apollo/variables.js
@@ -1,7 +1,21 @@
 import { makeVar } from "@apollo/client";
 
-export const darkModeVar = makeVar(
-  typeof localStorage !== "undefined"
-    ? localStorage.getItem("darkMode") === "true"
-    : false
-);
+const getDarkModeVar = () => {
+  if (typeof localStorage !== "undefined") {
+    const isDarkMode = localStorage.getItem("darkMode");
+
+    // Set dark mode by default
+    if (isDarkMode === null) {
+      document.documentElement.classList.add(["dark-theme"]);
+      document.documentElement.style.color = "#FFFFFF";
+      localStorage.setItem("darkMode", "true")
+      return true;
+    }
+
+    return isDarkMode === "true";
+  }
+  
+  return false;
+};
+
+export const darkModeVar = makeVar(getDarkModeVar());

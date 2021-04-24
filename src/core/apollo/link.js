236b002c7b787229889226dc95995978c861bd51
Matthew Lilley <hello@matthewlilley.com>
2021-04-24 18:18:22 +0100

    feat: polygon

--- src/core/apollo/link.js
+++ src/core/apollo/link.js
@@ -13,7 +13,7 @@ export const uniswap = from([
 export const exchange = from([
   new RetryLink(),
   new HttpLink({
-    uri: "https://api.thegraph.com/subgraphs/name/sushiswap/fantom-exchange",
+    uri: "https://api.thegraph.com/subgraphs/name/sushiswap/matic-exchange",
     shouldBatch: true,
   }),
 ]);
@@ -21,7 +21,7 @@ export const exchange = from([
 export const blocklytics = from([
   new RetryLink(),
   new HttpLink({
-    uri: "https://api.thegraph.com/subgraphs/name/matthewlilley/fantom-blocks",
+    uri: "https://api.thegraph.com/subgraphs/name/matthewlilley/polygon-blocks",
     shouldBatch: true,
   }),
 ]);

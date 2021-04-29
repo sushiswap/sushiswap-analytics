8ba1c266e990a4cd573da58cb85b946a974f41b4
Matthew Lilley <hello@matthewlilley.com>
2021-04-29 22:53:18 +0100

    feat: update logo url

--- src/components/TokenIcon.js
+++ src/components/TokenIcon.js
@@ -15,7 +15,7 @@ export default function TokenIcon({ id, ...rest }) {
   const classes = useStyles();
   const src = useMemo(
     () =>
-      `https://raw.githubusercontent.com/sushiswap/assets/master/blockchains/ethereum/assets/${toChecksumAddress(
+      `https://raw.githubusercontent.com/sushiswap/assets/master/blockchains/polygon/assets/${toChecksumAddress(
         id
       )}/logo.png`,
     [id]

76c8660137169e34b55431c772db171355c526e6
0xJiro <jiro@sushi.com>
2021-11-17 16:33:39 -0800

    swap out exchange subgraph endpoint

--- src/core/apollo/link.js
+++ src/core/apollo/link.js
@@ -13,7 +13,7 @@ export const uniswap = from([
 export const exchange = from([
   new RetryLink(),
   new HttpLink({
-    uri: "https://api.thegraph.com/subgraphs/name/jiro-ono/sushitestsubgraph",
+    uri: "https://api.thegraph.com/subgraphs/name/sushiswap/celo-exchange",
     shouldBatch: true,
   }),
 ]);

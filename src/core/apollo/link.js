8bc740cf0c3db9d07283a0167369bd789040fcec
0xJiro <jiro@sushi.com>
2021-11-19 02:30:08 -0800

    use moonriver exchange staging link

--- src/core/apollo/link.js
+++ src/core/apollo/link.js
@@ -13,7 +13,7 @@ export const uniswap = from([
 export const exchange = from([
   new RetryLink(),
   new HttpLink({
-    uri: "https://api.thegraph.com/subgraphs/name/sushiswap/moonriver-exchange",
+    uri: "https://api.thegraph.com/subgraphs/name/jiro-ono/moonriver-exchange-staging",
     shouldBatch: true,
   }),
 ]);

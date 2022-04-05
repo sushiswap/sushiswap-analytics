5b7fd68eac9131af4dcc3c748e0213e804af2ec4
0xJiro <jiro@sushi.com>
2022-04-05 13:51:11 -0400

    swap all graph endpoints to studio

--- src/core/apollo/link.js
+++ src/core/apollo/link.js
@@ -13,7 +13,7 @@ export const uniswap = from([
 export const bar = from([
   new RetryLink(),
   new HttpLink({
-    uri: `https://api.thegraph.com/subgraphs/name/sushiswap/sushi-bar`,
+    uri: `https://gateway.thegraph.com/api/${process.env.NEXT_PUBLIC_THE_GRAPH_API_KEY}/subgraphs/id/8Nwbz7YUtjf1NsD2mJUnuq5vWNbm2Gyag4nraLWyVJGi`,
     shouldBatch: true,
   }),
 ]);
@@ -21,7 +21,7 @@ export const bar = from([
 export const masterchef = from([
   new RetryLink(),
   new HttpLink({
-    uri: "https://api.thegraph.com/subgraphs/name/sushiswap/master-chef",
+    uri: `https://gateway.thegraph.com/api/${process.env.NEXT_PUBLIC_THE_GRAPH_API_KEY}/subgraphs/id/5mqK2me3PVLTUhMNESgAqq3SX26eGsMFwo8BLLZwNM4u`,
     shouldBatch: true,
   }),
 ]);
@@ -29,7 +29,7 @@ export const masterchef = from([
 export const exchange = from([
   new RetryLink(),
   new HttpLink({
-    uri: `https://api.thegraph.com/subgraphs/name/sushiswap/exchange`,
+    uri: `https://gateway.thegraph.com/api/${process.env.NEXT_PUBLIC_THE_GRAPH_API_KEY}/subgraphs/id/D7azkFFPFT5H8i32ApXLr34UQyBfxDAfKoCEK4M832M6`,
     shouldBatch: true,
   }),
 ]);
@@ -37,7 +37,7 @@ export const exchange = from([
 export const blocklytics = from([
   new RetryLink(),
   new HttpLink({
-    uri: "https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks",
+    uri: `https://gateway.thegraph.com/api/${process.env.NEXT_PUBLIC_THE_GRAPH_API_KEY}/subgraphs/id/3WFXNz46rk4iuVgsBybcGtxMa4cbHkBLfuSjUvvqs2MD`,
     shouldBatch: true,
   }),
 ]);

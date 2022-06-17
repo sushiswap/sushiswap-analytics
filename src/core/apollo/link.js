c2e6d6dc62d221c37f0700f97f75ee0017abc587
Matthew Lilley <hello@matthewlilley.com>
2022-06-17 07:37:18 +0100

    Update link.js

--- src/core/apollo/link.js
+++ src/core/apollo/link.js
@@ -13,7 +13,7 @@ export const uniswap = from([
 export const exchange = from([
   new RetryLink(),
   new HttpLink({
-    uri: "https://api.thegraph.com/subgraphs/name/sushiswap/arbitrum-exchange",
+    uri: "https://api.thegraph.com/subgraphs/name/sushiswap/exchange-arbitrum",
     shouldBatch: true,
   }),
 ]);
